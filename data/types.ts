20ef374074e31953bff7273b49647cddad8a373f
alphadevking <danielose266@gmail.com>
2023-03-11 20:38:25 +0100

    updated the signup api, trying to find the source of the map error

---
+++ data/types.ts
@@ -0,0 +1,7 @@
+export interface FormInputs {
+    id: number;
+    firstname: string;
+    lastname: string;
+    email: string;
+    password: string;
+}
--- src/pages/api/signup.ts
+++ src/pages/api/signup.ts
@@ -1,29 +1,43 @@
-import fs from 'fs';
-import path from 'path';
+import { FormInputs } from '../../../data/types';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-type FormInputs = {
-    firstname: string;
-    lastname: string;
-    email: string;
-    password: string;
-};
+interface RequestBody {
+    formInputs: FormInputs[];
+}
 
-const formInputFilePath = path.join(process.cwd(), 'public', 'formInputs.json');
-
-const handler = (req: NextApiRequest, res: NextApiResponse): void => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
-        const formInputs: FormInputs = JSON.parse(req.body);
+        try {
+            const { formInputs } = req.body as RequestBody;
+            console.log('formInputs:', formInputs);
 
-        fs.writeFile(formInputFilePath, JSON.stringify(formInputs), (err) => {
-            if (err) {
-                console.error(err);
-                res.status(500).json({ error: 'Failed to write form data to file.' });
-            } else {
-                console.log('Form data successfully written to file.');
-                res.status(200).json({ message: 'Form data successfully submitted.' });
-            }
-        });
+            // Generate unique id for each form input
+            const formInputFilePath = '/api/formInputs.json';
+            const existingFormInputs: FormInputs[] = await fetch(formInputFilePath)
+                .then(res => res.json())
+                .catch(() => []);
+
+            const lastId = existingFormInputs.reduce((maxId: number, formInput: FormInputs) => Math.max(maxId, formInput.id), 0);
+
+            const formInputsWithIds = formInputs.map((formInput, i) => ({
+                ...formInput,
+                id: lastId + i + 1,
+            }));
+
+            await fetch(formInputFilePath, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify([...existingFormInputs, ...formInputsWithIds])
+            });
+
+            console.log('Form data successfully written to file.');
+            res.status(200).json({ message: 'Form data successfully submitted.' });
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Failed to write form data to file.' });
+        }
     } else {
         res.status(404).json({ error: 'API route not found.' });
     }
