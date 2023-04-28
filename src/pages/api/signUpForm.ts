e417c64e3a356903b41925f7fc1446ec08aae71f
alphadevking <danielose266@gmail.com>
2023-04-28 23:21:25 +0100

    added the id generation script, schema complete

--- src/pages/api/signUpForm.ts
+++ src/pages/api/signUpForm.ts
@@ -3,6 +3,7 @@ import fs from 'fs';
 import path from 'path';
 
 interface FormEntry {
+    id: string;
     firstname: string;
     lastname: string;
     email: string;
@@ -14,25 +15,38 @@ interface FormResponse {
     formData?: FormEntry[];
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<FormResponse>) {
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<FormResponse>
+) {
     const formData: FormEntry[] = getFormData();
     const dataFilePath = path.join(process.cwd(), 'data', 'formData.json');
 
     if (req.method === 'POST') {
         const { firstname, lastname, email, password }: FormEntry = req.body;
-        const newFormEntry: FormEntry = { firstname, lastname, email, password };
-
-        console.log('Received new form entry:', newFormEntry);
+        const newFormEntry: FormEntry = {
+            id: generateId(),
+            firstname,
+            lastname,
+            email,
+            password,
+        };
 
         formData.push(newFormEntry);
         saveFormData(formData, dataFilePath);
 
         res.status(200).json({ message: 'Form data submitted successfully.' });
     } else if (req.method === 'GET') {
-        res.status(200).json({ message: 'Form data retrieved successfully.', formData });
+        res
+            .status(200)
+            .json({ message: 'Form data retrieved successfully.', formData });
     } else {
         res.status(404).json({ message: 'Invalid API request.' });
     }
+}
+
+function generateId(): string {
+    return Math.random().toString(36).substr(2, 9);
 }
 
 function getFormData(): FormEntry[] {
